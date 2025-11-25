05762fe343f7ea1e494245bdac702948991ca822
fayzullah1987 <fayzullah1987@gmail.com>
2025-11-25 17:02:51 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,30 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
-// Fix __dirname in ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Load .env from parent folder
-dotenv.config({
-  path: path.resolve(__dirname, '..', '.env')
-});
-
-// Debug
-console.log('Loaded BACKEND URL:', process.env.VITE_BACKEND_URL);
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: process.env.VITE_BACKEND_URL,
+        target: 'https://video-streaming-04z5.onrender.com',
         changeOrigin: true
       }
     }
